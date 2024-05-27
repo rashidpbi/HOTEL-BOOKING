7a46dc9b697856e0060f6be50f5398c44557f1ad
rashidpbi <rashidpbi786@gmail.com>
2024-05-27 15:59:24 +0530

    simple changes

---
+++ src/reducer/fetchReducer.ts
@@ -0,0 +1,23 @@
+/* import { useReducer } from "react"
+
+enum actions{
+FETCH = 'FETCH'
+}
+interface Action{
+    type: actions,
+    payload?:string
+}
+interface State{
+
+}
+const fetchReducer = (state:State,action:Action)=>{
+
+    switch(action.type){
+        case actions.FETCH:{
+            return 
+        }
+    }
+
+}
+
+ */
